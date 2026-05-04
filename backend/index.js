864215fcd2a794df168d48461c286bfac1682e1b
anukya reddy <areddy@anukyas-air.wifi.uc.edu>
2026-05-04 18:54:07 -0400

    Fix CORS for production

--- backend/index.js
+++ backend/index.js
@@ -26,7 +26,7 @@ app.use(
 );
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL,
+    origin: ["https://course-selling-application-bay.vercel.app", "http://localhost:5173"],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
